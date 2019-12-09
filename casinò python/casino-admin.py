98aa6333c01523fb4fee9e9c555e8fede026f74f
abissues <53044683+Abissues@users.noreply.github.com>
2019-12-09 19:11:56 +0100

    Update casino-admin.py

--- casinò python/casino-admin.py
+++ casinò python/casino-admin.py
@@ -1,5 +1,5 @@
 import random
-import requests
+#import requests
 import os
 import socket
 import hashlib
@@ -12,7 +12,7 @@ import tkinter as tk
 from tkinter import messagebox
 
 
-ip = requests.get("https://api.myip.com")
+#ip = requests.get("https://api.myip.com")
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
 F = socket.gethostname()
@@ -38,14 +38,14 @@ def logadmin(): #logadmin
                             print("nome utente non corretto ")
                         with open('accesso.txt') as file:
                             testate = open('accesso.txt', 'a')
-                            testate.write("attenzione rilevato con password errato alle " + tempo + '\n' + "Dati " + '\n' + ip.text + hostname + IPAddr + platform.platform())
+                            testate.write("attenzione rilevato con password errato alle " + tempo + '\n' + "Dati " + '\n'  + hostname + IPAddr + platform.platform())
                             testate.close()
             else:
                 print("nome utente non corretto ")
                 tempo = str(datetime.datetime.now())
             with open('accesso.txt') as file:
                 testate = open('accesso.txt', 'a')
-                testate.write('\n' + "attenzione rilevato con nome errato alle " + tempo + '\n' + "Dati " + '\n' + ip.text + '\n' + 'Nome pc ' + hostname + '\n' + 'IP del pc ' + IPAddr + '\n' + 'OS ' + platform.platform())
+                testate.write('\n' + "attenzione rilevato con nome errato alle " + tempo + '\n' + "Dati "  + '\n' + 'Nome pc ' + hostname + '\n' + 'IP del pc ' + IPAddr + '\n' + 'OS ' + platform.platform())
                 testate.close()
                 exit()
                 
@@ -70,13 +70,24 @@ def unban():
         for line in lines:
             if line.strip("\n") != x:
                 f.write(line)
+def ammonizioni():
+    f=open("ammonizioni.txt","r")
+    a=f.readline()
+    a=int(a)
+    f.close()
+    f=open("ammonizioni.txt","w")
+    ab=a+1
+    f.write(str(ab))
+    f.close()
 def admin():
     lista()
-    scelta = int(input("1-ban,2-sban "))
+    scelta = int(input("1-ban,2-sban 3.ammonizioni"))
     if scelta == 1:
         bannare()
     elif scelta == 2:
         unban()
+    elif scelta == 3:
+        ammonizioni()
 
 def main():
     try:
@@ -85,13 +96,13 @@ def main():
         root = tk.Tk()
         root.withdraw()
         MsgBox = tk.messagebox.askquestion ('Exit Application','è stato rilevato un errore vuoi loggarlo',icon = 'error')
-    if MsgBox == 'yes':
-        tk.messagebox.showinfo('Exit','puoi trovare il log a nel file errore.txt' )
-        root.destroy()
-        log = open("errore.txt",'a')
-        log.write('\n' + tempo + "si è verificato un errore ")
-        log.close()
-    else:
-        tk.messagebox.showinfo('Exit','Grazie lo stesso')
+        if MsgBox == 'yes':
+            tk.messagebox.showinfo('Exit','puoi trovare il log a nel file errore.txt' )
+            root.destroy()
+            log = open("errore.txt",'a')
+            log.write('\n' + tempo + "si è verificato un errore ")
+            log.close()
+        else:
+            tk.messagebox.showinfo('Exit','Grazie lo stesso')
 
 main()
